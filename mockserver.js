35f5f31a28dcd7c180e2c174418530229ce1464c
Yannik Schiebelhut <yannik.schiebelhut@gmail.com>
2020-11-10 12:13:55 +0100

    added test cases and implementation for SetRobotStatus

--- mockserver.js
+++ mockserver.js
@@ -602,7 +602,65 @@ require('node-ui5/factory')({
             })
 
             return true
-        };
+		};
+		
+		var SetRobotStatus = function(oXhr, sUrlParams) {
+			console.log("invoking SetRobotStatus")
+			console.log("sUrlParams: " + sUrlParams)
+			var oUrlParams = sUrlParams.split("&").reduce(function(prev, curr, i, arr) {
+                var p = curr.split("=")
+                prev[decodeURIComponent(p[0])] = decodeURIComponent(p[1]).replace(/\'/g, '')
+                return prev
+			}, {})
+			console.log("oUrlParams: " + JSON.stringify(oUrlParams))
+			var uri = ""
+			var abort = false
+
+			// 1. Check if the robot resource exists in EWM
+            // yes: continue
+            // no: return business_error: ROBOT_NOT_FOUND
+            uri = "/odata/SAP/ZEWM_ROBCO_SRV/RobotSet(Lgnum='" + oUrlParams.Lgnum + "',Rsrc='" + oUrlParams.Rsrc + "')"
+            console.log("checking if robot resource exists at: " + uri)
+            jQuery.ajax({
+                url: uri,
+                dataType: 'json',
+                async: false,
+                success: function(res) {
+                    console.log("found that robot resource " + oUrlParams.Rsrc + " exists")
+                },
+                error: function(err) {
+                    console.log(JSON.stringify(err))
+                    console.log("robot resource " + oUrlParams.Rsrc + " does not exist")
+                    oXhr.respondJSON(404, {}, { "error": { "code": "ROBOT_NOT_FOUND" } })
+                    abort = true
+                }
+            })
+            if (abort)
+                return true
+
+
+			// 2. Check if parameter "ExccodeOverall" has the right length
+			// yes: set status for robot (and return robot?)
+			// no: return business_error: ROBOT_STATUS_NOT_SET
+			if (oUrlParams.ExccodeOverall) {
+				if (oUrlParams.ExccodeOverall.length <= 4) {
+					jQuery.ajax({
+						url: uri, // set at 1.
+						method: 'PUT',
+						data: JSON.stringify(oUrlParams),
+						async: false,
+						success: function(res) {
+							oXhr.respondJSON(200, {}, res)
+						},
+						error: function(err) {
+							console.log(JSON.stringify(err))
+						}
+					})
+				}
+			}
+			// else respond with error
+			oXhr.respondJSON(404, {}, { "error": { "code": "ROBOT_STATUS_NOT_SET" } })
+		}
 
 
         // End of function imports
@@ -654,7 +712,12 @@ require('node-ui5/factory')({
             method: "POST",
             path: "SendFirstConfirmationError\\?(.*)",
             response: SendFirstConfirmationError
-        })
+		})
+		aRequests.push({
+			method: "POST",
+			path: "SetRobotStatus\\?(.*)",
+			response: SetRobotStatus
+		})
         ms.setRequests(aRequests);
 
 
